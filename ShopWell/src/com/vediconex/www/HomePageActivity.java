33f855ca3d0d77f6429077aa2dc5fb4e736af809
patrick <developer.session@gmail.com>
2014-05-27 11:23:14 +0100

    first commit

---
+++ ShopWell/src/com/vediconex/www/HomePageActivity.java
@@ -0,0 +1,15 @@
+package com.vediconex.www;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class HomePageActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_page);
+    }
+}
